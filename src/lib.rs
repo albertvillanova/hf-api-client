7c239f07221b19738ed59af7f458b0490a4b1085
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-09-05 22:13:41 +0200

    Implement list_repo_tree

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
 use reqwest::Client;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RepoTreeItem {
+    pub r#type: String,
+    pub oid: String,
+    pub size: u64,
+    pub path: String,
+}
 
 pub struct HfApiClient {
     client: Client,
@@ -11,5 +20,12 @@ impl HfApiClient {
             client: Client::new(),
             base_url: String::from("https://huggingface.co/api"),
         }
+    }
+
+    pub async fn list_repo_tree(&self) -> Result<Vec<RepoTreeItem>, reqwest::Error> {
+        let url = format!("{}/datasets/hf-internal-testing/dataset_with_data_files/tree/main", self.base_url);
+        let response = self.client.get(&url).send().await?;
+        let repo_tree_items: Vec<RepoTreeItem> = response.json().await?;
+        Ok(repo_tree_items)
     }
 }

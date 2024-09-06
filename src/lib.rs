6a09e916e92e32ea596604551295ba1997c93d1c
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-09-06 08:04:59 +0200

    Add doc for list_repo_tree

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,29 @@ impl HfApiClient {
         }
     }
 
+    /// Lists the repo's tree (files and directories) at a given reference.
+    ///
+    /// # Example
+    /// ```rust
+    /// use hf_api_client::HfApiClient;
+    /// use tokio;
+    ///
+    /// #[tokio::main]
+    /// async fn main() {
+    ///     let client = HfApiClient::new();
+    ///     let repo_tree = client.list_repo_tree().await.unwrap();
+    ///     println!("{:#?}", repo_tree);
+    /// }
+    /// ```
+    ///
+    /// # Errors
+    /// This function will return an error if the request fails or the response cannot be deserialized.
+    ///
+    /// # Panics
+    /// This function will panic if the base URL is invalid.
+    ///
+    /// # Returns
+    /// A vector of `RepoTree` objects representing the files and directories in the repo.
     pub async fn list_repo_tree(&self) -> Result<Vec<RepoTree>, reqwest::Error> {
         let url = format!("{}/datasets/hf-internal-testing/dataset_with_data_files/tree/main", self.base_url);
         let response = self.client.get(&url).send().await?;
